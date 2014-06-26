9bcb41118274e7c39aab29a6de77b86fc3b7b065
Larry Fantasy <larry.fantasy@gmail.com>
2014-06-26 09:52:03 -0700

    basic build

--- Gruntfile.js
+++ Gruntfile.js
@@ -381,7 +381,7 @@ module.exports = function (grunt) {
         'uglify',
         'copy:dist',
         'modernizr',
-        'rev',
+        // 'rev',
         'usemin',
         'htmlmin'
     ]);
