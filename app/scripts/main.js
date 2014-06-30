a4818659adb4e1db5dd0f06f9d76925b8ae7b28b
Larry Fantasy <larry.fantasy@gmail.com>
2014-06-29 18:05:29 -0700

    swipe and pan

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,105 @@
 console.log('\'Allo \'Allo!');
 
-var Phresh = Ember.Application.create();
+var App = Ember.Application.create();
 
-Phresh.Router.map(function () {
-  this.route('splash',{path:"/splash"});
-  this.route('posts',{path:"/posts"});
+App.Router.map(function () {
+	this.route('index',{path:'/'});
+  this.route('splash');
+  this.route('posts');
+  this.route('tutorial');
+  this.route('discovery');
+  this.route('logout');
+  this.route('showroom');
 });
 
+App.IndexRoute = Ember.Route.extend({
+	afterModel: function(){
+		console.log('index');
+	}
+});
+
+App.LogoutRoute = Ember.Route.extend({
+	afterModel: function(){
+		console.log('logout');
+		FB.logout(function(response) {
+		  // user is now logged out
+		  console.log(response.status);
+		});
+	}
+});
+
+App.TutorialRoute = Ember.Route.extend({
+	afterModel: function(){
+		console.log('tutorial');
+	}
+});
+
+App.swipeView = Ember.View.extend({
+  templateName: 'swipe',
+
+  swipeOptions: {
+    direction: Em.OneGestureDirection.Left | Em.OneGestureDirection.Right,
+    cancelPeriod: 100,
+    swipeThreshold: 10
+  },
+
+  touchMove: function(event) {
+    event.preventDefault();
+  },
+
+  swipeEnd: function(recognizer, evt) {
+    var direction = recognizer.get('swipeDirection');
+    console.log(evt);
+    if (direction === Em.OneGestureDirection.Right) {
+      console.log('right');
+    } else if (direction === Em.OneGestureDirection.Left) {
+      console.log('left');
+    }
+  }
+});
+
+App.panView = Ember.View.extend({
+  templateName: 'pan',
+  panOptions: {
+    numberOfRequiredTouches: 1
+  },
+
+  panChange: function(rec, evt) {
+		console.log(evt);
+    // var SMOOTH_FACTOR = 5,
+        // x, y,
+    var val = rec.get('translation');
+        // canvasContext = this.get('canvasContext'),
+        // image = this.image;
+
+    // x = Math.min(this.x + SMOOTH_FACTOR * val.x,
+    //   image.width - canvasContext.canvas.width * this.scale);
+
+    // y = Math.min(this.y + SMOOTH_FACTOR * val.y,
+    //   image.height - canvasContext.canvas.height * this.scale);
+
+    // x = Math.max(x, 0);
+    // y = Math.max(y, 0);
+
+    // this.x = x;
+    // this.y = y;
+
+    // this.refreshImage();
+    console.log(val.x, val.y);
+  },
+
+  touchMove: function(event) {
+    event.preventDefault();
+  }
+
+});
 
 
 $(function () {
   $('#FBButton').click(function () {
-
+		console.log('click');
+		FB.login(function(response){
+			console.log(response);
+		},{scope:'public_profile,email,user_friends'});
   });
 });
--- app/scripts/vendor/fb.js
+++ app/scripts/vendor/fb.js
@@ -1,17 +1,18 @@
 // This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
   console.log('statusChangeCallback');
-  console.log(response);
+  console.log(response.status);
   // The response object is returned with a status field that lets the
   // app know the current login status of the person.
   // Full docs on the response object can be found in the documentation
   // for FB.getLoginStatus().
   if (response.status === 'connected') {
     // Logged into your app and Facebook.
-    testAPI();
+    window.location = "/#/discovery";
   } else if (response.status === 'not_authorized') {
-    // window.location = "/#/splash";
+    window.location = "/#/splash";
   } else {
+    window.location = "/#/splash";
   }
 }
 
