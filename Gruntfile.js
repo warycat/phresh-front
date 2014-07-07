4042fe846e63333aebe2459053be72578da22eae
Larry Fantasy <larry.fantasy@gmail.com>
2014-07-06 22:30:50 -0700

    add cname

--- Gruntfile.js
+++ Gruntfile.js
@@ -284,7 +284,8 @@ module.exports = function (grunt) {
             '.htaccess',
             'images/{,*/}*.*',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'CNAME'
           ]
         }, {
           expand: true,
