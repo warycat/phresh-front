19e0aaab80b1853e2a4861e9c276d429956fb880
Larry Fantasy <larry.fantasy@gmail.com>
2014-07-15 13:05:15 -0700

    add main to index

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,7 +16,6 @@ var App = Ember.Application.create();
 
 App.Router.map(function () {
 	this.route('index',{path:'/'});
-  this.route('facebook',{path:'facebook'});
   this.route('main');
   this.route('main-description');
   this.route('showroom');
@@ -25,107 +24,107 @@ App.Router.map(function () {
 });
 
 
-App.LogoutRoute = Ember.Route.extend({
-	afterModel: function(){
-		console.log('logout');
-		FB.logout(function(response) {
-		  // user is now logged out
-		  console.log(response.status);
-		});
-	}
-});
+// App.LogoutRoute = Ember.Route.extend({
+// 	afterModel: function(){
+// 		console.log('logout');
+// 		FB.logout(function(response) {
+// 		  // user is now logged out
+// 		  console.log(response.status);
+// 		});
+// 	}
+// });
 
-App.TutorialRoute = Ember.Route.extend({
-	afterModel: function(){
-		console.log('tutorial');
-	}
-});
+// App.TutorialRoute = Ember.Route.extend({
+// 	afterModel: function(){
+// 		console.log('tutorial');
+// 	}
+// });
 
-App.DiscoveryRoute = Ember.Route.extend({
-  model:function(){
-    return Ember.$.getJSON('http://node.whatisphresh.com/scan');
-  }
-});
+// App.DiscoveryRoute = Ember.Route.extend({
+//   model:function(){
+//     return Ember.$.getJSON('http://node.whatisphresh.com/scan');
+//   }
+// });
 
-App.swipeView = Ember.View.extend({
-  templateName: 'swipe',
+// App.swipeView = Ember.View.extend({
+//   templateName: 'swipe',
 
-  swipeOptions: {
-    direction: Em.OneGestureDirection.Left | Em.OneGestureDirection.Right,
-    cancelPeriod: 100,
-    swipeThreshold: 10
-  },
+//   swipeOptions: {
+//     direction: Em.OneGestureDirection.Left | Em.OneGestureDirection.Right,
+//     cancelPeriod: 100,
+//     swipeThreshold: 10
+//   },
 
-  touchMove: function(event) {
-    event.preventDefault();
-  },
+//   touchMove: function(event) {
+//     event.preventDefault();
+//   },
 
-  swipeEnd: function(recognizer, evt) {
-    var direction = recognizer.get('swipeDirection');
-    console.log(evt);
-    if (direction === Em.OneGestureDirection.Right) {
-      console.log('right');
-    } else if (direction === Em.OneGestureDirection.Left) {
-      console.log('left');
-    }
-  }
-});
+//   swipeEnd: function(recognizer, evt) {
+//     var direction = recognizer.get('swipeDirection');
+//     console.log(evt);
+//     if (direction === Em.OneGestureDirection.Right) {
+//       console.log('right');
+//     } else if (direction === Em.OneGestureDirection.Left) {
+//       console.log('left');
+//     }
+//   }
+// });
 
-App.panView = Ember.View.extend({
-  templateName: 'pan',
-  panOptions: {
-    numberOfRequiredTouches: 1
-  },
+// App.panView = Ember.View.extend({
+//   templateName: 'pan',
+//   panOptions: {
+//     numberOfRequiredTouches: 1
+//   },
 
-  didInsertElement:function(){
-		// console.log('didInsertElement');
-		// getMoreItems(10,true);
-  },
+//   didInsertElement:function(){
+// 		// console.log('didInsertElement');
+// 		// getMoreItems(10,true);
+//   },
   
-  touchStart: function(evt){
-    var originalEvent = evt.originalEvent;
-    console.log(originalEvent);
-    sum = 0;
-  },
+//   touchStart: function(evt){
+//     var originalEvent = evt.originalEvent;
+//     console.log(originalEvent);
+//     sum = 0;
+//   },
 
-  touchEnd: function(){
-    sum = 0;
-    $('.top').css({
-      'left': (10 + sum/10) + '%',
-      'opacity': 1,
-      '-webkit-transform': 'rotate(' + (sum)/40 + 'deg)',
-      'transform': 'rotate(' + (sum)/40 + 'deg)',
-      'border-color':'#000000',
-      'border-width':'1px'
-    });
-    getNextOnline(0, false);
-  },
+//   touchEnd: function(){
+//     sum = 0;
+//     $('.top').css({
+//       'left': (10 + sum/10) + '%',
+//       'opacity': 1,
+//       '-webkit-transform': 'rotate(' + (sum)/40 + 'deg)',
+//       'transform': 'rotate(' + (sum)/40 + 'deg)',
+//       'border-color':'#000000',
+//       'border-width':'1px'
+//     });
+//     getNextOnline(0, false);
+//   },
 
-  panChange: function(rec) {
-    var val = rec.get('translation');
-    sum += val.x;
-    $('.top').css({
-      'left': (10 + sum/10) + '%',
-      'opacity': 1- Math.abs(sum/1000),
-      '-webkit-transform': 'rotate(' + (sum)/40 + 'deg)',
-      'transform': 'rotate(' + (sum)/40 + 'deg)',
-      'border-color':(sum<0)?'#EF4242':'#2eF131',
-      'border-width':'30px'
-    });
-  },
+//   panChange: function(rec) {
+//     var val = rec.get('translation');
+//     sum += val.x;
+//     $('.top').css({
+//       'left': (10 + sum/10) + '%',
+//       'opacity': 1- Math.abs(sum/1000),
+//       '-webkit-transform': 'rotate(' + (sum)/40 + 'deg)',
+//       'transform': 'rotate(' + (sum)/40 + 'deg)',
+//       'border-color':(sum<0)?'#EF4242':'#2eF131',
+//       'border-width':'30px'
+//     });
+//   },
 
-  touchMove: function(event) {
-    event.preventDefault();
-  }
+//   touchMove: function(event) {
+//     event.preventDefault();
+//   }
 
-});
+// });
 
-$(function () {
-  $('#FBButton').click(function () {
-    console.log('click');
-    FB.login(function(response){
-      console.log(response);
-    },{scope:'public_profile,email,user_friends'});
-  });
-});
+// $(function () {
+//   $('#FBButton').click(function () {
+//     console.log('click');
+//     FB.login(function(response){
+//       console.log(response);
+//     },{scope:'public_profile,email,user_friends'});
+//   });
+// });
 
